237862100b4acaedb7c981fb83cd6cfbb4c03c4e
OKPooja <pooja.gawade213046@tsecol>
2023-09-16 09:55:51 +0530

    banking page updated

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -521,6 +521,9 @@ class _HomeScreenState extends State<HomeScreen> {
                         ],
                       ),
                     ),
+                    const SizedBox(
+                      height: 20
+                    )
                   ],
                 ),
               ),
